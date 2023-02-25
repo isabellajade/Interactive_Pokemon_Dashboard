0b9b418ab3c90fc7def83cc6d15f3f55eaaf5266
Siskeaux <114033750+Siskeaux@users.noreply.github.com>
2023-02-25 17:53:49 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,49 +1,37 @@
+# import dependencies
+
 import numpy as np
 import pymongo
-
-# import sqlalchemy
-# from sqlalchemy.ext.automap import automap_base
-# from sqlalchemy.orm import Session
-# from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
 from flask_cors import CORS
 
 import json
 from pprint import pprint
+import requests
 
-url = "https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json"
+# assigning url
+
+#url = "https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json"
+
+# making get request
 
 #pokemonDataResponse = requests.get(url)
+#pokemonData = pokemonDataResponse.json()
+
+#New MongoDB Pokeman Database; replaces pulling from previous GitHub .json file - William
 
 client = pymongo.MongoClient("mongodb+srv://admin:project123@tccedxproject3group3.kbc4ahv.mongodb.net/pokemon_data?retryWrites=true&w=majority")
 db = client["pokemon_data"]
 
 pokemonData = list(db["pokemon_collection"].find())
 
-#################################################
-# Database Setup
-#################################################
-# engine = create_engine("sqlite:///titanic.sqlite")
-
-# # reflect an existing database into a new model
-# Base = automap_base()
-# # reflect the tables
-# Base.prepare(autoload_with=engine)
-
-# # Save reference to the table
-# Passenger = Base.classes.passenger
-
-#################################################
 # Flask Setup
-#################################################
+
 app = Flask(__name__)
 CORS(app)
 
-
-#################################################
 # Flask Routes
-#################################################
 
 @app.route("/")
 def welcome():
@@ -53,7 +41,7 @@ def welcome():
         f"/api/v1.0/type_counts<br/>"
     )
 
-
+# selecting pokemon types data
 @app.route("/api/v1.0/type_counts")
 def type_counts():
     typeDict = {}
@@ -79,42 +67,7 @@ def type_counts():
         "size": size,
     }
     return ret
-    # Create our session (link) from Python to the DB
-    # session = Session(engine)
-
-    # """Return a list of all passenger names"""
-    # # Query all passengers
-    # results = session.query(Passenger.name).all()
-
-    # session.close()
-
-    # # Convert list of tuples into normal list
-    # all_names = list(np.ravel(results))
-
-    # return jsonify(all_names)
-
-
-# @app.route("/api/v1.0/passengers")
-# def passengers():
-#     # Create our session (link) from Python to the DB
-#     session = Session(engine)
-
-#     """Return a list of passenger data including the name, age, and sex of each passenger"""
-#     # Query all passengers
-#     results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
-
-#     session.close()
-
-#     # Create a dictionary from the row data and append to a list of all_passengers
-#     all_passengers = []
-#     for name, age, sex in results:
-#         passenger_dict = {}
-#         passenger_dict["name"] = name
-#         passenger_dict["age"] = age
-#         passenger_dict["sex"] = sex
-#         all_passengers.append(passenger_dict)
-
-#     return jsonify(all_passengers)
+   
 
 
 if __name__ == '__main__':
